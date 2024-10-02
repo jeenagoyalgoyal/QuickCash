d282b4119d9de80760a289d1439008a27384ab0f
jgoyal <jn640254@dal.ca>
2024-10-02 00:22:39 -0300

    test case

--- app/src/androidTest/java/com/example/quickcash/UserRoleTest.java
+++ app/src/androidTest/java/com/example/quickcash/UserRoleTest.java
@@ -19,26 +19,24 @@ public class UserRoleTest {
     public ActivityScenarioRule<RoleActivity> activity = new ActivityScenarioRule<>(RoleActivity.class);
 
     @Test
-    public void testRoleSwitching(){
+    public void testRoleSwitching() {
 
         //Test for initial role as employee
         Espresso.onView(ViewMatchers.withId(R.id.welcomeText)).check(ViewAssertions.matches(ViewMatchers.withText("Welcome, employee")));
-
         //Click on the button to switch
         Espresso.onView(ViewMatchers.withId(R.id.roleSwitch)).perform(ViewActions.click());
-
         //Check role is employer now
         Espresso.onView(ViewMatchers.withId(R.id.welcomeText)).check(ViewAssertions.matches(ViewMatchers.withText("Welcome, employer")));
-
         //Click to switch role again
         Espresso.onView(ViewMatchers.withId(R.id.roleSwitch)).perform(ViewActions.click());
-
         // Check back to employee
         Espresso.onView(ViewMatchers.withId(R.id.welcomeText)).check(ViewAssertions.matches(ViewMatchers.withText("Welcome, employee")));
     }
 
+
+    // Tests for updation on button text based on correct role assigned
     @Test
-    public void testBtnTextSwitch(){
+    public void testBtnTextSwitch() {
         Espresso.onView(ViewMatchers.withId(R.id.roleSwitch)).check(ViewAssertions.matches(ViewMatchers.withText("Switch to employer")));
         Espresso.onView(ViewMatchers.withId(R.id.roleSwitch)).perform(ViewActions.click());
         Espresso.onIdle();
@@ -46,4 +44,22 @@ public class UserRoleTest {
         Espresso.onView(ViewMatchers.withId(R.id.roleSwitch)).perform(ViewActions.click());
         Espresso.onView(ViewMatchers.withId(R.id.roleSwitch)).check(ViewAssertions.matches(ViewMatchers.withText("Switch to employer")));
     }
+
+    //Test Multiple Switches
+    @Test
+    public void testMultipleSwitches(){
+        Espresso.onView(ViewMatchers.withId(R.id.welcomeText)).check(ViewAssertions.matches(ViewMatchers.withText("Welcome, employee")));
+        Espresso.onView(ViewMatchers.withId(R.id.roleSwitch)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.welcomeText)).check(ViewAssertions.matches(ViewMatchers.withText("Welcome, employer")));
+        Espresso.onView(ViewMatchers.withId(R.id.roleSwitch)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.welcomeText)).check(ViewAssertions.matches(ViewMatchers.withText("Welcome, employee")));
+        Espresso.onView(ViewMatchers.withId(R.id.roleSwitch)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.welcomeText)).check(ViewAssertions.matches(ViewMatchers.withText("Welcome, employer")));
+
+
+    }
 }
+
+
+    //Test for elements for UI are shown or hidden specific to the role
+
