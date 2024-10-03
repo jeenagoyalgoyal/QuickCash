eefff3cc32e60d57510e7ef9c8bbae0225193512
Dhruv Sharma <dh554929@dal.ca>
2024-10-03 19:23:27 -0300

    refactor: Made Espresso static import so functions can be used without referencing Espresso

--- app/src/androidTest/java/com/example/quickcash/registrationEspressoTest.java
+++ app/src/androidTest/java/com/example/quickcash/registrationEspressoTest.java
@@ -1,16 +1,13 @@
 package com.example.quickcash;
 
+import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
 import androidx.test.core.app.ActivityScenario;
 import org.junit.Before;
-import androidx.test.espresso.Espresso;
-
 import org.junit.Test;
 
 public class registrationEspressoTest {
@@ -24,23 +21,23 @@ public class registrationEspressoTest {
 
     @Test
     public void validNameTest() {
-        Espresso.onView(withId(R.id.enterName)).perform(typeText("example name"));
-        Espresso.onView(withId(R.id.buttonRegister)).perform(click());
-        Espresso.onView(withId(R.id.validName)).check(matches(withText("Valid Name")));
+        onView(withId(R.id.enterName)).perform(typeText("example name"));
+        onView(withId(R.id.buttonRegister)).perform(click());
+        onView(withId(R.id.validName)).check(matches(withText("Valid Name")));
     }
 
     @Test
     public void validEmailTest() {
-        Espresso.onView(withId(R.id.enterEmail)).perform(typeText("example@email.com"));
-        Espresso.onView(withId(R.id.buttonRegister)).perform(click());
-        Espresso.onView(withId(R.id.validEmail)).check(matches(withText("Valid Email")));
+        onView(withId(R.id.enterEmail)).perform(typeText("example@email.com"));
+        onView(withId(R.id.buttonRegister)).perform(click());
+        onView(withId(R.id.validEmail)).check(matches(withText("Valid Email")));
     }
 
     @Test
     public void validPasswordTest() {
-        Espresso.onView(withId(R.id.enterPassword)).perform(typeText("example@email.com"));
-        Espresso.onView(withId(R.id.buttonRegister)).perform(click());
-        Espresso.onView(withId(R.id.validPassword)).check(matches(withText("Valid Password")));
+        onView(withId(R.id.enterPassword)).perform(typeText("example@email.com"));
+        onView(withId(R.id.buttonRegister)).perform(click());
+        onView(withId(R.id.validPassword)).check(matches(withText("Valid Password")));
     }
 
     @Test
