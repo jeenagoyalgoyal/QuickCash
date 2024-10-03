6a4c379f75f902fe2924b37e9cb6d413b049ead9
Dhruv Sharma <dh554929@dal.ca>
2024-10-03 14:53:33 -0300

    build: Created RegistrationActivity class for User Registration logic

--- app/src/androidTest/java/com/example/quickcash/registrationEspressoTest.java
+++ app/src/androidTest/java/com/example/quickcash/registrationEspressoTest.java
@@ -1,8 +1,20 @@
 package com.example.quickcash;
 
+import android.telephony.ims.RegistrationManager;
+
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class registrationEspressoTest {
 
-    @Test
+    private activityScenario<RegistrationActivity> regActivityScenario;
+
+    @Before
+    public void setup(){
+        regActivityScenario = ActivityScenario.launch(RegistrationActivity.class);
+    }
+    
+
 }
