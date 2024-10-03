51020e3a57a1a6560415045eea1e90ce422eb63b
Satwik Gorla <st325640@dal.ca>
2024-10-03 16:02:17 -0300

    test: added espresso test for valid name

--- app/src/androidTest/java/com/example/quickcash/registrationEspressoTest.java
+++ app/src/androidTest/java/com/example/quickcash/registrationEspressoTest.java
@@ -1,8 +1,16 @@
 package com.example.quickcash;
 
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import androidx.test.core.app.ActivityScenario;
 import org.junit.Before;
 import androidx.test.espresso.Espresso;
+
 import org.junit.Test;
 
 public class registrationEspressoTest {
@@ -15,7 +23,9 @@ public class registrationEspressoTest {
     }
 
     @Test
-    public void nameTest() {
-
+    public void validNameTest() {
+        Espresso.onView(withId(R.id.enterName)).perform(typeText("example name"));
+        Espresso.onView(withId(R.id.buttonRegister)).perform(click());
+        Espresso.onView(withId(R.id.validName)).check(matches(withText("Valid")));
     }
 }
