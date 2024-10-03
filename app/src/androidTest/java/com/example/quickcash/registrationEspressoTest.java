13a2ac8460c664c2762305bea0757daf8b17df3a
Satwik Gorla <st325640@dal.ca>
2024-10-03 14:46:25 -0300

    created layout with fields and radio buttons

--- app/src/androidTest/java/com/example/quickcash/registrationEspressoTest.java
+++ app/src/androidTest/java/com/example/quickcash/registrationEspressoTest.java
@@ -1,8 +1,13 @@
 package com.example.quickcash;
 
+import androidx.test.espresso.Espresso;
+
 import org.junit.Test;
 
 public class registrationEspressoTest {
 
     @Test
+    public void nameTest() {
+        Espresso.onView();
+    }
 }
