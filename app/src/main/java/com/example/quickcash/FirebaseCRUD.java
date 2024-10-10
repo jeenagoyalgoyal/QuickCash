7eef7cc3662a36e9f9516424c864917f789e678e
Dhruv Sharma <dh554929@dal.ca>
2024-10-09 23:17:18 -0300

    build: Fixed Android Gradle version to be 8.5.2 instead of 8.6

--- app/src/main/java/com/example/quickcash/FirebaseCRUD.java
+++ app/src/main/java/com/example/quickcash/FirebaseCRUD.java
@@ -1,17 +1,22 @@
 package com.example.quickcash;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FirebaseCRUD {
-
-    private FirebaseDatabase database;
-    private DatabaseReference bannerIDRef = null;
+    private final FirebaseDatabase database;
+    
+    private DatabaseReference passwordRef = null;
     private DatabaseReference emailRef = null;
-    private DatabaseReference roleRef = null;
-    private String extractedEmailAddress = null;
-    private String extractedRole = null;
-    private String extractedBannerID = null;
+    private DatabaseReference Name = null;
+
+    private String extractedEmailAddress;
+    private String extractedPassword;
 
 
     public FirebaseCRUD(FirebaseDatabase database) {
@@ -20,11 +25,59 @@ public class FirebaseCRUD {
         this.initializeDatabaseRefListeners();
     }
 
-    private void initializeDatabaseRefListeners() {
-        //Do something
+    protected void initializeDatabaseRefs() {
+        this.passwordRef = getPasswordRef();
+        this.emailRef = getEmailAddressRef();
     }
 
-    private void initializeDatabaseRefs() {
-        //Do something
+
+    protected DatabaseReference getEmailAddressRef() {
+        return this.database.getReference("emailAddress");
     }
+
+    protected DatabaseReference getPasswordRef(){
+        return this.database.getReference("password");
+    }
+
+
+    protected void initializeDatabaseRefListeners() {
+        this.setEmailListener();
+        this.setPasswordListener();
+    }
+
+
+    protected void setEmailListener() {
+        this.emailRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                extractedEmailAddress = snapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    protected void setPasswordListener() {
+        this.passwordRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                extractedPassword = snapshot.getValue(String.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+    public String getExtractedEmailAddress() {
+        return this.extractedEmailAddress;
+    }
+
+    public String getExtractedPassword() {
+        return this.extractedPassword;
+    }
+
 }
