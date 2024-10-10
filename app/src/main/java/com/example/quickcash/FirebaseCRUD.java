3fc34675015e4c6299234f5209f7f83929d010a1
Jonathan Ayoub <jonathan-ayoub@hotmail.com>
2024-10-09 23:13:59 -0300

    fix: ensure getExtractedPassword returns correct value after Firebase retrieval

--- app/src/main/java/com/example/quickcash/FirebaseCRUD.java
+++ app/src/main/java/com/example/quickcash/FirebaseCRUD.java
@@ -6,17 +6,15 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class FirebaseCRUD {
     private final FirebaseDatabase database;
-    
+
     private DatabaseReference passwordRef = null;
     private DatabaseReference emailRef = null;
 
     private String extractedEmailAddress;
     private String extractedPassword;
-
 
     public FirebaseCRUD(FirebaseDatabase database) {
         this.database = database;
@@ -29,25 +27,21 @@ public class FirebaseCRUD {
         this.emailRef = getEmailAddressRef();
     }
 
-
     protected DatabaseReference getEmailAddressRef() {
         return this.database.getReference("emailAddress");
     }
 
-    protected DatabaseReference getPasswordRef(){
+    protected DatabaseReference getPasswordRef() {
         return this.database.getReference("password");
     }
 
-
     protected void initializeDatabaseRefListeners() {
-        //Incomplete method, add the missing code.
         this.setEmailListener();
         this.setPasswordListener();
     }
 
-
     protected void setEmailListener() {
-        this.emailRef.addValueEventListener(new ValueEventListener() {
+        this.emailRef.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 extractedEmailAddress = snapshot.getValue(String.class);
@@ -55,12 +49,13 @@ public class FirebaseCRUD {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+                // Handle possible errors here
             }
         });
     }
 
     protected void setPasswordListener() {
-        this.passwordRef.addValueEventListener(new ValueEventListener() {
+        this.passwordRef.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 extractedPassword = snapshot.getValue(String.class);
@@ -68,6 +63,7 @@ public class FirebaseCRUD {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+                // Handle possible errors here
             }
         });
     }
@@ -77,8 +73,10 @@ public class FirebaseCRUD {
     }
 
     public String getExtractedPassword() {
-        //buggy code, fix the bug!
-        return this.extractedPassword;
+        if (extractedPassword != null) {
+            return this.extractedPassword;
+        } else {
+            return "Password not yet available";
+        }
     }
-
 }
