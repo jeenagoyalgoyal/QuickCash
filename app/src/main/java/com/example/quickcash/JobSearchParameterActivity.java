be7bb3ce85aca5e8acd0cad16a7cb2910f3c7926
alanj <al471151@dal.ca>
2024-10-24 20:57:40 -0300

    feat 10.1 implemented logic for search job button

---
+++ app/src/main/java/com/example/quickcash/JobSearchParameterActivity.java
@@ -0,0 +1,14 @@
+package com.example.quickcash;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JobSearchParameterActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
