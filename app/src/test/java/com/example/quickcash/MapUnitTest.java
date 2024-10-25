1aa65a0f40875c88efff11aa00a486c6c4f534a0
Dhruv Sharma <dh554929@dal.ca>
2024-10-25 17:17:19 -0300

    refactor: refactored assert statements to have correct formatting

--- app/src/test/java/com/example/quickcash/MapUnitTest.java
+++ app/src/test/java/com/example/quickcash/MapUnitTest.java
@@ -4,20 +4,27 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MapUnitTest {
+    //To be checked:
+    //Job title
+    //Salary
+    //Duration
+    //Location
+    //Add to perfect job
 
     @Test
-    public void TestValidJobTitle(){
+    public void testValidJobTitle(){
         // Assuming getJobTitle takes name as input
         // and returns job title for job posted by given name
         // Can be changed depending on how we are getting job title for pin on map
 
         //Also change activity to correct getter activity if changed
-        assertEquals(MapActivity.getJobTitle("John"), "Barber");
+        assertEquals("Barber", MapActivity.getJobTitle("John"));
     }
 
     @Test
-    public void TestInvalidJobTitle(){
-        assertNotEquals(MapActivity.getJobTitle("John"), "null");
+    public void testInvalidJobTitle(){
+        assertNotEquals("null", MapActivity.getJobTitle("John"));
     }
+
 
 }
