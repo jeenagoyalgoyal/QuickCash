b4bf6b51c76ac88a34f302f7108e8d8157e60805
Satwik Gorla <st325640@dal.ca>
2024-10-26 14:50:41 -0300

    feat: created files and setup for Junit and UI tests, ready for pair programming

---
+++ app/src/test/java/com/example/quickcash/PrefEmployerEmployeeJunit.java
@@ -0,0 +1,15 @@
+package com.example.quickcash;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PrefEmployerEmployeeJunit {
+
+    @Test
+    public void dummyTest(){
+        assertEquals(2+2, 4);
+    }
+}
