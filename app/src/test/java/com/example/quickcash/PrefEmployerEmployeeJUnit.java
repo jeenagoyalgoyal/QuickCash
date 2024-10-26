96bef37e3e8ede6b9d5bbe7026db6b91fc797631
Satwik Gorla <st325640@dal.ca>
2024-10-26 14:54:23 -0300

    fix: renamed JUnit file for consistency

--- app/src/test/java/com/example/quickcash/PrefEmployerEmployeeJUnit.java
+++ app/src/test/java/com/example/quickcash/PrefEmployerEmployeeJUnit.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class PrefEmployerEmployeeJunit {
+public class PrefEmployerEmployeeJUnit {
 
     @Test
     public void dummyTest(){
