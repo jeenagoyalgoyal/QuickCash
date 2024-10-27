29740b13ccad1f8293cac1f95f968f7c652f4f14
alanj <al471151@dal.ca>
2024-10-26 23:33:14 -0300

    feat 9.2 Wrote ui test case for required field and placeholders

--- app/src/androidTest/java/com/example/quickcash/JobSubmissionUITest.java
+++ app/src/androidTest/java/com/example/quickcash/JobSubmissionUITest.java
@@ -9,6 +9,7 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
@@ -17,6 +18,7 @@ import androidx.test.espresso.Espresso;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.assertion.ViewAssertions;
 import androidx.test.espresso.matcher.ViewMatchers;
+import static org.hamcrest.Matchers.equalTo;
 
 
 import org.junit.Before;
@@ -29,24 +31,59 @@ import org.junit.runners.JUnit4;
 public class JobSubmissionUITest {
 
 
-    public ActivityScenario<RoleActivity> activityScenario;
+    public ActivityScenario<RoleActivity> roleActivityActivityScenario;
+    public ActivityScenario<JobSubmission> jobSubmissionActivityScenario;
 
-    @Before
-    public void setup() {
-        activityScenario = ActivityScenario.launch(RoleActivity.class);
+
+    public void setupRoleActivity() {
+        roleActivityActivityScenario = ActivityScenario.launch(RoleActivity.class);
+    }
+
+    public void setupJobSubmissionActivityScenario(){
+        jobSubmissionActivityScenario = ActivityScenario.launch(JobSubmission.class);
     }
 
     @Test
     public void checkCreateJobButtonPresent(){
+        setupRoleActivity();
         onView(withText("Switch to employer")).perform(click());
         onView(withText("Create Job")).check(matches(isDisplayed()));
     }
 
     @Test
     public void checkJobSubmissionForm(){
+        setupRoleActivity();
         onView(withText("Switch to employer")).perform(click());
         onView(withText("Create Job")).perform(click());
         onView(withText("Job Submission Form")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkRequiredFields(){
+        setupJobSubmissionActivityScenario();
+        onView(withId(R.id.jobTitle)).check(matches(isDisplayed()));
+        onView(withId(R.id.companyName)).check(matches(isDisplayed()));
+        onView(withId(R.id.spinnerJobType)).check(matches(isDisplayed()));
+        onView(withId(R.id.requirementText)).check(matches(isDisplayed()));
+        onView(withId(R.id.editTextNumber)).check(matches(isDisplayed()));
+        onView(withId(R.id.spinnerUrgency)).check(matches(isDisplayed()));
+        onView(withId(R.id.locationJob)).check(matches(isDisplayed()));
+        onView(withId(R.id.expectedDuration)).check(matches(isDisplayed()));
+        onView(withId(R.id.startDate)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testJobTypeDefaultPlaceholder() {
+        setupJobSubmissionActivityScenario();
+        onView(withId(R.id.spinnerJobType))
+                .check(ViewAssertions.matches(withSpinnerText(equalTo("Select job type"))));
+    }
+
+    @Test
+    public void testUrgencyDefaultPlaceholder() {
+        setupJobSubmissionActivityScenario();
+        onView(withId(R.id.spinnerUrgency))
+                .check(ViewAssertions.matches(withSpinnerText(equalTo("Select urgency"))));
     }
 
 }
