d70182112fec5b29c66170c9a6218e9e0758ce7d
aanam <ab766748@dal.ca>
2024-10-26 23:58:22 -0300

    feat: PreferredJobs initialisation

--- app/src/main/java/com/example/quickcash/PreferredJobs.java
+++ app/src/main/java/com/example/quickcash/PreferredJobs.java
@@ -1,18 +1,23 @@
 package com.example.quickcash;
 
-public class PreferredJobs {
-    //created for test purposes, modify as necessary
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-    public void addJob(TempJob job, String userID){
-        //adds job to preferred jobs for given user to database
+public class PreferredJobs {
+    private DatabaseReference databaseReference;
+
+    public PreferredJobs() {
+        // Initialize Firebase Database reference
+        this.databaseReference = FirebaseDatabase.getInstance().getReference("preferred_jobs");
     }
 
-    public TempJob[] getJobs(String userID){
-        //returns jobs under User ID in form of array, change as per your implementation
-        return null;
+    public void addJob(TempJob job, String userID) {
+        // Add job under the user's preferred jobs in the database
+
     }
 
     public void removeJob(TempJob job, String userID) {
-        //removes job from preferred jobs for given user from database
+        // Remove job from the user's preferred jobs in the database
+
     }
 }
