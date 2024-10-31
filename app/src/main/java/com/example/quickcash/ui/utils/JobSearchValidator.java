33e26ebeb376d903073b7ac5a106d1505b1a3e82
Dhruv Sharma <dh554929@dal.ca>
2024-10-30 23:12:33 -0300

    feat: Added a JobSearchValidator.java class which validates parameters entered on search screen

---
+++ app/src/main/java/com/example/quickcash/ui/utils/JobSearchValidator.java
@@ -0,0 +1,46 @@
+package com.example.quickcash.ui.utils;
+
+
+public class JobSearchValidator {
+
+    public static boolean isValidJobTitle(String jobTitle){
+        return jobTitle != null && !jobTitle.trim().isEmpty();
+    }
+
+    public static boolean isValidSalaryString(String minSalary, String maxSalary){
+        //If both empty return true
+        if(minSalary.isEmpty() && maxSalary.isEmpty()){
+            return true;
+            //If minSalary is empty and maxSalary is more than 0
+        } else if (minSalary.isEmpty()) {
+            if(Integer.parseInt(maxSalary) > 0){
+                return true;
+            }
+            //If maxSalary is empty and minSalary is more than 0
+        } else if (maxSalary.isEmpty()) {
+            if(Integer.parseInt(minSalary) > 0){
+                return true;
+            }
+        }
+
+        //If both not empty, then check for validity.
+        return isValidSalaryRange(Integer.parseInt(minSalary), Integer.parseInt(maxSalary));
+    }
+
+    public static boolean isValidSalaryRange(int minSalary, int maxSalary){
+        return (minSalary >= 0 && maxSalary >= 0 && minSalary <= maxSalary );
+    }
+
+    public static boolean isValidDuration(String duration){
+        return duration != null && !duration.trim().isEmpty();
+    }
+
+    public static boolean isValidLocation(String location){
+        return location != null && !location.trim().isEmpty();
+    }
+
+    public static boolean allEmptyFields(){
+        return false;
+    }
+
+}
