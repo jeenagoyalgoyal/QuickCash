ba32331ede136892f2388b8556f31d711e05dd58
Satwik Gorla <st325640@dal.ca>
2024-11-03 15:08:28 -0400

    fix: changed ui tests to better match current implementation of feature

--- app/src/androidTest/java/com/example/quickcash/PreferredJobsUITest.java
+++ app/src/androidTest/java/com/example/quickcash/PreferredJobsUITest.java
@@ -47,20 +47,16 @@ public class PreferredJobsUITest {
         passwordBox.setText("Test_Pass123#");
         UiObject registerButton = device.findObject(new UiSelector().text("Login"));
         registerButton.clickAndWaitForNewWindow();
-
-        //Switching role from employer to employee
-        UiObject switchButton = device.findObject(new UiSelector().text("Switch to Employee"));
-        switchButton.clickAndWaitForNewWindow();
-
         //looking for 'Search Jobs' button on homepage
         UiObject searchJobButton = device.findObject(new UiSelector().text("My Preferred Jobs"));
+        assertTrue("My preferred jobs button should be present on employee dashboard", searchJobButton.exists());
         searchJobButton.clickAndWaitForNewWindow();
 
-        //looking for 'options' button on a single job listing
+        //looking for 'View Job' button on a single job listing
         UiObject optionsButton = device.findObject(new UiSelector().text("View Job"));
         optionsButton.click();
 
-        //looking for 'options' button on a single job listing
+        //looking for 'Close' button on a single job listing
         UiObject closeButton = device.findObject(new UiSelector().text("Close"));
         closeButton.click();
     }
@@ -75,10 +71,6 @@ public class PreferredJobsUITest {
         UiObject registerButton = device.findObject(new UiSelector().text("Login"));
         registerButton.clickAndWaitForNewWindow();
 
-        //Switching role from employer to employee
-        UiObject switchButton = device.findObject(new UiSelector().text("Switch to Employee"));
-        switchButton.clickAndWaitForNewWindow();
-
         //looking for 'Search Jobs' button on homepage
         UiObject searchJobButton = device.findObject(new UiSelector().text("Search Job"));
         searchJobButton.clickAndWaitForNewWindow();
@@ -88,11 +80,13 @@ public class PreferredJobsUITest {
 
         //Press Search
         UiObject searchButton = device.findObject(new UiSelector().text("Search"));
-        searchButton.clickAndWaitForNewWindow();
+        searchButton.longClick();
 
         //looking for 'options' button on a single job listing
-        UiObject optionsButton = device.findObject(new UiSelector().text("Add To My Preferred Jobs"));
-        optionsButton.click();
+        UiObject optionsButton = device.findObject(new UiSelector().text("Options"));
+        optionsButton.longClick();
 
+        UiObject addToPreferredJobsButton = device.findObject(new UiSelector().text("Add to Preferred Jobs"));
+        assertTrue("Add to preferred jobs must show up in the popup menu when options is clicked on a job listing", addToPreferredJobsButton.exists());
     }
 }
