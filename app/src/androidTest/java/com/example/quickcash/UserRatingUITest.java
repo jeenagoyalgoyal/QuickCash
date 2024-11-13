fc48bb9aa15ac2d9981ec8ac273701d2f702edbb
Satwik Gorla <st325640@dal.ca>
2024-11-12 20:05:38 -0400

    fix: test files werent pushed correctly, attempting second commit

--- app/src/androidTest/java/com/example/quickcash/UserRatingUITest.java
+++ app/src/androidTest/java/com/example/quickcash/UserRatingUITest.java
@@ -1,2 +1,49 @@
-package com.example.quickcash;public class UserRatingUITest {
+package com.example.quickcash;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
+import androidx.test.uiautomator.Until;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class UserRatingUITest {
+    private static final int LAUNCH_TIMEOUT = 120;
+    final String launcherPackageName = "com.example.quickcash";
+    private UiDevice device;
+
+
+    @Before
+    public void setup() {
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        Context context = ApplicationProvider.getApplicationContext();
+        Intent launcherIntent = context.getPackageManager().getLaunchIntentForPackage(launcherPackageName);
+        launcherIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(launcherIntent);
+        device.wait(Until.hasObject(By.pkg(launcherPackageName).depth(0)), LAUNCH_TIMEOUT);
+    }
+
+    //test to make sure UIAutomator runs correctly and app starts up correctly
+    @Test
+    public void checkIfLandingPageIsVisible() {
+        UiObject emailBox = device.findObject(new UiSelector().text("Email"));
+        assertTrue(emailBox.exists());
+        UiObject passwordBox = device.findObject(new UiSelector().text("Password"));
+        assertTrue(passwordBox.exists());
+    }
 }
--- app/src/test/java/com/example/quickcash/UserRatingJUnitTest.java
+++ app/src/test/java/com/example/quickcash/UserRatingJUnitTest.java
@@ -1,2 +1,13 @@
-package com.example.quickcash;public class UserRatingJUnitTest {
+package com.example.quickcash;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserRatingJUnitTest {
+
+    @Test
+    public void dummyTest() {
+        assertEquals(2,2);
+    }
 }
