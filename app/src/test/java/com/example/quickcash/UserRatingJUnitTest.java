8426b4308f1cb771f631fbeecaddb16966c2dfee
Satwik Gorla <st325640@dal.ca>
2024-11-13 22:39:52 -0400

    test: written test for star component testing

--- app/src/test/java/com/example/quickcash/UserRatingJUnitTest.java
+++ app/src/test/java/com/example/quickcash/UserRatingJUnitTest.java
@@ -1,13 +1,26 @@
 package com.example.quickcash;
 
+import org.mockito.Mockito;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import android.widget.RatingBar;
+
 public class UserRatingJUnitTest {
 
+    //it is assumed that star rating component will be implemented using RatingBar Ui element, if you use another method to implement
+    //then change test as needed
     @Test
-    public void dummyTest() {
-        assertEquals(2,2);
+    public void starRatingComponentTest() {
+        //replace this mock with RatingBar UI element.
+        //mock start
+        RatingBar starRatingComponent = Mockito.mock(RatingBar.class);
+        Mockito.when(starRatingComponent.getRating()).thenReturn((float) 0);
+        //mock end
+
+        starRatingComponent.setRating(5);
+        assertEquals("After rating is set to 5.0, getRating should return 5.0",5,starRatingComponent.getRating(),0);
     }
 }
