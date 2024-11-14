1c91777b8d1355707621901522754d0dc0472064
Satwik Gorla <st325640@dal.ca>
2024-11-13 23:09:46 -0400

    test: written test for comment field for UserRating and reworked how mocks are intialized

---
+++ app/src/main/java/com/example/quickcash/UserRatingHelper.java
@@ -0,0 +1,11 @@
+package com.example.quickcash;
+
+public interface UserRatingHelper {
+    float getRating();
+
+    void setRating(float i);
+
+    String getComment();
+
+    void setComment(String testing);
+}
--- app/src/test/java/com/example/quickcash/UserRatingJUnitTest.java
+++ app/src/test/java/com/example/quickcash/UserRatingJUnitTest.java
@@ -1,26 +1,47 @@
 package com.example.quickcash;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import org.junit.Test;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.*;
 
-import android.widget.RatingBar;
+import android.widget.*;
 
 public class UserRatingJUnitTest {
 
-    //it is assumed that star rating component will be implemented using RatingBar Ui element, if you use another method to implement
-    //then change test as needed
+    //currently class is mocked using an interface and Mockito
+    @Mock
+    UserRatingHelper userRatingHelper;
+
+    //initializes mocks
+    @Before
+    public void setup(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    //it is assumed that a helper class is used to handle RatingBar and EditText on the comment page.
     @Test
     public void starRatingComponentTest() {
-        //replace this mock with RatingBar UI element.
-        //mock start
-        RatingBar starRatingComponent = Mockito.mock(RatingBar.class);
-        Mockito.when(starRatingComponent.getRating()).thenReturn((float) 0);
-        //mock end
+        //mocked method returns
+        Mockito.when(userRatingHelper.getRating()).thenReturn((float) 0);
 
-        starRatingComponent.setRating(5);
-        assertEquals("After rating is set to 5.0, getRating should return 5.0",5,starRatingComponent.getRating(),0);
+        userRatingHelper.setRating(5F);
+
+        assertEquals("After rating is set to 5.0, getRating should return 5.0",5F,userRatingHelper.getRating(),0);
+    }
+
+    @Test
+    public void commentFieldTest() {
+        //mocked method returns
+        Mockito.when(userRatingHelper.getComment()).thenReturn("");
+
+        userRatingHelper.setComment("testing");
+
+        assertEquals("After comment is set, getComment should return the set comment","testing",userRatingHelper.getComment());
     }
 }
