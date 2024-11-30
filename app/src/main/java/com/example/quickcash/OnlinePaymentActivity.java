6725ec07bf8490aa80012f0bb94e71372c0d4a7b
Satwik Gorla <st325640@dal.ca>
2024-11-30 19:09:12 -0400

    feat: added ui needed for online payment feature

--- app/src/main/java/com/example/quickcash/OnlinePaymentActivity.java
+++ app/src/main/java/com/example/quickcash/OnlinePaymentActivity.java
@@ -1,9 +1,11 @@
 package com.example.quickcash;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,9 +14,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class OnlinePaymentActivity extends AppCompatActivity {
 
     private Button paymentButton;
+    private Button selectJobButton;
+    private Dialog dialog;
+    private ImageButton crossButton;
+    private FirebaseAuth mAuth;
+    private String email;
+    private String userID;
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -26,8 +36,27 @@ public class OnlinePaymentActivity extends AppCompatActivity {
             return insets;
         });
 
+        setCurrentUserID();
         //setup UI
         setupPaymentButton();
+        setupSelectJobButton();
+        setupDialog();
+    }
+    
+    private void setCurrentUserID(){
+        this.mAuth = FirebaseAuth.getInstance();
+        this.email = mAuth.getCurrentUser() != null ? mAuth.getCurrentUser().getEmail() : null;
+        this.userID = sanitizeEmail(email);
+    }
+
+    private void setupSelectJobButton() {
+        selectJobButton = findViewById(R.id.jobSelectButton);
+        selectJobButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchJobSelectDialog();
+            }
+        });
     }
 
     protected void setupPaymentButton(){
@@ -40,7 +69,25 @@ public class OnlinePaymentActivity extends AppCompatActivity {
         });
     }
 
+    private void setupDialog() {
+        dialog = new Dialog(this);
+        dialog.setContentView(R.layout.preferred_employer_jobs_dialog_box);
+        crossButton = dialog.findViewById(R.id.crossButton);
+        crossButton.setOnClickListener(v -> {
+            dialog.cancel();
+        });
+    }
+
+    private void launchJobSelectDialog() {
+        dialog.show();
+    }
+    
+    
     protected void paypalPayment(){
         //does paypal payment stuff
+    }
+
+    private String sanitizeEmail(String email) {
+        return email.replace(".", ",");
     }
 }
